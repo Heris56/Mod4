7827f4e7cbc969f5811fae212c8410580e09d6f7
Haikal Risnandar <115600487+Heris56@users.noreply.github.com>
2024-03-13 05:08:46 +0700

    Add files via upload
    Menambahkan Table Driven

---
+++ tpMod4_1302220150/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpMod4_1302220150
+{
+    public class KodePos()
+    {
+        int x;
+    }
+}
---
+++ tpMod4_1302220150/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
+using System;
+
+
+KodePos nt = new KodePos();
+Console.WriteLine(nt.getKodePos("Kujangsari"));
+Console.WriteLine("Hello, World!");
+
+
+public class KodePos
+{
+    private Dictionary<string, string> kodePosData;
+
+    public KodePos()
+    {
+        // Inisialisasi data kode pos
+        kodePosData = new Dictionary<string, string>();
+        kodePosData["Batununggal"] = "40266";
+        kodePosData["Kujangsari"] = "40287";
+        kodePosData["Mengger"] = "40267";
+        kodePosData["Wates"] = "40256";
+        kodePosData["Cijaura"] = "40287";
+        kodePosData["Jatisari"] = "40286";
+        kodePosData["Margasari"] = "40286";
+        kodePosData["Sekejati"] = "40286";
+        kodePosData["Kebonwaru"] = "40272";
+        kodePosData["Maleer"] = "40274";
+        kodePosData["Samoja"] = "40273";
+    }
+    public string getKodePos(string kelurahan)
+    {
+        if (kodePosData.ContainsKey(kelurahan))
+        {
+            return kodePosData[kelurahan];
+        }
+        else
+        {
+            return "Kode pos tidak ditemukan untuk kelurahan " + kelurahan;
+        }
+    }
+};
+
+public class DoorMachine
+{
+
+}
